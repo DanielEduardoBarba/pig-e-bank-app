f1b2528378cc8f9673dccca5c133b51f5d979f30
Daniel Barba <frdb103@gmail.com>
2023-03-13 14:24:26 -0400

    better charts

--- src/scenes/Metrics.jsx
+++ src/scenes/Metrics.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import ChartsEmbedSDK from '@mongodb-js/charts-embed-dom';
-
+import "./Metrics.css"
 export default function Metrics(){
 
     useEffect(()=>{
@@ -10,18 +10,18 @@ export default function Metrics(){
         });
         
         // embed a chart
-        const chart = sdk.createChart({
+        const chart1 = sdk.createChart({
           chartId: '640d599e-554b-4d3e-8905-0508f96da2c8',
         });
-        const chart1 = sdk.createChart({
+        const chart2 = sdk.createChart({
           chartId: '640d6762-9d2d-48e0-8dd4-02e6c383cdd1',
         });
 
-        chart
-          .render(document.getElementById('chart'))
-          .catch(() => window.alert('Chart failed to initialize'));
         chart1
           .render(document.getElementById('chart1'))
+          .catch(() => window.alert('Chart failed to initialize'));
+        chart2
+          .render(document.getElementById('chart2'))
           .catch(() => window.alert('Chart failed to initialize'));
         
         // refresh the chart whenever #refreshButton is clicked
@@ -43,8 +43,11 @@ export default function Metrics(){
 
     return(
         <>
-          <div style={{height:"500px", margin:"20px"}}id="chart"/>
-          <div style={{height:"500px",  margin:"20px"}}id="chart1"/>
+        <div className="Metrics">
+          
+          <div style={{height:"500px", margin:"20px"}}id="chart1"/>
+          <div style={{height:"400px",  margin:"20px"}}id="chart2"/>
+        </div>
         {/* <button style={{height:"20px"}}id="refreshButton">Refresh</button> */}
         {/* <div style={{height:"200px"}} id="dashboard"/> */}
         </>
